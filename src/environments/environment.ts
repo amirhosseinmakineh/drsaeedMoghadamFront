c62f23d295c5f463a55f72d7263522d56220a8eb
amirhosseinmakineh <amirhosseinmakineh1379@gmail.com>
2026-07-09 20:14:05 +0330

    Fix offline and realtime push notifications (v3)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: "https://drsaeedback.drmoghadam.runflare.run/api",
+  apiBaseUrl: "http://localhost:5182/api",
   webPushVapidPublicKey:
     "BHrRTag6eomjzkRjtPB4PUKv7RWx08MpTtBslDRei-oev6Ka3ivekjg3Y8GcEf3VZYNxCFW1dYoiewFU5huPiAA",
 };
