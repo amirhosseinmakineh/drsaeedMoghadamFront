589c24c3c5de4f51903c654c9cf8857a058ec7a1
amirhosseinmakineh <amirhosseinmakineh1379@gmail.com>
2026-07-30 21:54:35 +0330

    change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl:'https://api.drsaeedmoghadam.com/api',
+  apiBaseUrl:'http://localhost:5182/api',
   webPushVapidPublicKey: "BHrRTag6eomjzkRjtPB4PUKv7RWx08MpTtBslDRei-oev6Ka3ivekjg3Y8GcEf3VZYNxCFW1dYoiewFU5huPiAA",
 };
