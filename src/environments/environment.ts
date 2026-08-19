a968422792d9ce7e3dc622a2da602c59aeae293d
amirhosseinmakineh <amirhosseinmakineh1379@gmail.com>
2026-08-19 18:07:18 +0330

    fix bug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiBaseUrl:'http://localhost:5182/api',
   webPushVapidPublicKey:
-    "BHrRTag6eomjzkRjtPB4PUKv7RWx08MpTtBslDRei-oev6Ka3ivekjg3Y8GcEf3VZYNxCFW1dYoiewFU5huPiAA"  ,
+    "BHrRTag6eomjzkRjtPB4PUKv7RWx08MpTtBslDRei-oev6Ka3ivekjg3Y8GcEf3VZYNxCFW1dYoiewFU5huPiAA"   ,
 };
