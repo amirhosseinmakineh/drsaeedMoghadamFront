79679877e89cc9bfcaad9ddc2c555f3403654e5b
amirhosseinmakineh <amirhosseinmakineh1379@gmail.com>
2026-08-19 20:09:55 +0330

    first commit on stage

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+
+  apiBaseUrl: "/api",
+
+  webPushVapidPublicKey:
+    "BHrRTag6eomjzkRjtPB4PUKv7RWx08MpTtBslDRei-oev6Ka3ivekjg3Y8GcEf3VZYNxCFW1dYoiewFU5huPiAA",
+};
